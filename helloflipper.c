7ea6f3e8e0953ece595ad901562fe5527e3ab500
NeedSomeData <i@needsomedata.online>
2023-05-13 23:15:00 +0300

    Update helloflipper.c

--- helloflipper.c
+++ helloflipper.c
@@ -3,6 +3,7 @@
 
 #include <gui/gui.h>
 #include <input/input.h>
+#include <notification/notification-messages.h>
 
 #include "helloflipper_icons.h"
 
@@ -10,7 +11,14 @@ typedef struct {
     uint8_t x, y;
 } ImagePosition;
 
+typedef struct {
+    uint8_t x, y;
+    bool active;
+    uint32_t timer;
+} PixelProjectile;
+
 static ImagePosition image_position = {.x = 0, .y = 0};
+static PixelProjectile pixel_projectile = {.x = 0, .y = 0, .active = false, .timer = 0};
 
 // Screen is 128x64 px
 static void app_draw_callback(Canvas* canvas, void* ctx) {
@@ -18,6 +26,9 @@ static void app_draw_callback(Canvas* canvas, void* ctx) {
 
     canvas_clear(canvas);
     canvas_draw_icon(canvas, image_position.x % 128, image_position.y % 64, &I_airplane);
+    if (pixel_projectile.active) {
+        canvas_draw_pixel(canvas, pixel_projectile.x, pixel_projectile.y);
+    }
 }
 
 static void app_input_callback(InputEvent* input_event, void* ctx) {
@@ -25,6 +36,20 @@ static void app_input_callback(InputEvent* input_event, void* ctx) {
 
     FuriMessageQueue* event_queue = ctx;
     furi_message_queue_put(event_queue, input_event, FuriWaitForever);
+}
+
+static void update_pixel_projectile(uint32_t elapsed_time) {
+    if (pixel_projectile.active) {
+        pixel_projectile.timer += elapsed_time;
+        if (pixel_projectile.timer >= 500) {
+            pixel_projectile.timer -= 500;
+            pixel_projectile.x += 1;
+        }
+
+        if (pixel_projectile.x >= 128) {
+            pixel_projectile.active = false;
+        }
+    }
 }
 
 int32_t helloflipper_main(void* p) {
@@ -39,10 +64,12 @@ int32_t helloflipper_main(void* p) {
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
     InputEvent event;
+    uint32_t elapsed_time;
 
     bool running = true;
     while(running) {
-        if(furi_message_queue_get(event_queue, &event, 100) == FuriStatusOk) {
+        elapsed_time = 100;
+        if(furi_message_queue_get(event_queue, &event, elapsed_time) == FuriStatusOk) {
             if((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
                 switch(event.key) {
                 case InputKeyLeft:
@@ -57,21 +84,16 @@ int32_t helloflipper_main(void* p) {
                 case InputKeyDown:
                     image_position.y += 2;
                     break;
+                case InputKeyOk:
+                    if (!pixel_projectile.active) {
+                        pixel_projectile.x = image_position.x % 128;
+                        pixel_projectile.y = image_position.y % 64;
+                        pixel_projectile.active = true;
+                    }
+                    break;
                 default:
                     running = false;
                     break;
                 }
             }
-        }
-        view_port_update(view_port);
-    }
-
-    view_port_enabled_set(view_port, false);
-    gui_remove_view_port(gui, view_port);
-    view_port_free(view_port);
-    furi_message_queue_free(event_queue);
-
-    furi_record_close(RECORD_GUI);
-
-    return 0;
-}
+       
